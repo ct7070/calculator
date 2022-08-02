a5478e2a289cad5e377d9e64909daa1db618abbc
scuby <c7573167070@gmail.com>
2022-08-01 20:44:02 -0400

    Basic operations logic, html divs and minor styling

---
+++ script.js
@@ -0,0 +1,24 @@
+// calculator
+
+let result = 0;
+
+function addition(num1, num2){
+    result = num1 + num2;
+   return result;
+}
+
+function subtraction(num1, num2){
+    result = num1 - num2;
+    return result;
+}
+
+function multiplication(num1, num2){
+    result = num1 * num2;
+    return result;
+}
+
+function division(num1, num2){
+    result = num1 / num2;
+    return result;
+}
+
