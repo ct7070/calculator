16ad6ec92b292c80d6d376b3affebf7ea18e971a
scuby <c7573167070@gmail.com>
2022-08-23 16:07:58 -0400

    Validity checks for keypresses, results history aesthetics, and math operations calls

--- script.js
+++ script.js
@@ -1,115 +1,190 @@
 // cros calculator clone
 
 let result = 0;
+let operator = "";
 let btnDecimalActive = true;
-let haveArg = false;
+let symbol = "";
 
+const btns = document.querySelector('#buttons');
+const ops = document.querySelectorAll('#operators div');
+
+const firstArgument = document.createElement('span');
+const newOp = document.createElement('span');
+const insertOp = document.createElement('span');
+
+const screenSpacer = document.querySelector('#screenSpacer');
+const screenParent = document.getElementById('screenSpacer').parentElement;
+
+const btnPress = btns.addEventListener('click', (key) => {
+    const results = document.querySelectorAll('.result');
+    const btnPress = key.target.textContent;
+    const btnClass = key.target.className;
+
+    if (btnClass == "clear"){
+        fullReset();
+    } else if (results.length > 0 && newOp.textContent == ''){
+        for (const op of ops){
+            if (op.classList == btnClass){ updateOperator(btnClass); } 
+        } 
+    } else {
+        insertOp.id = "newEntry";
+        screenSpacer.appendChild(insertOp);
+        validateKeypress(btnPress, btnClass);
+    }
+});
+    
+function validateKeypress(btnPress, btnClass){
+    const printNextArg = document.createElement('span');
+    if(!btnClass){  // button is a number
+        firstArgument.textContent += btnPress;
+        insertOp.appendChild(firstArgument);
+    } else if(btnClass == "decimal"){
+        if(btnDecimalActive == true){
+            if(firstArgument.textContent == ""){
+                firstArgument.textContent += "0.";
+                insertOp.appendChild(firstArgument);
+                btnDecimalActive = false;
+            } else {
+                firstArgument.textContent += ".";
+                insertOp.appendChild(firstArgument);
+                btnDecimalActive = false;
+            }
+        }
+    }
+    else if(btnClass == "backspace"){
+        let backUp = firstArgument.textContent;
+        if((backUp.length == 1) || (backUp == "0.") || (backUp == "")){
+            firstArgument.textContent = "";
+            insertOp.appendChild(firstArgument);
+        } else {
+            firstArgument.textContent = firstArgument.textContent.slice(0, -1);
+            insertOp.appendChild(firstArgument);
+        }
+        if(firstArgument.textContent.includes('.')){ btnDecimalActive = false;
+        } else { btnDecimalActive = true; }
+    } else {    // button is an operator
+        const getResults = document.querySelectorAll('.result');
+        const lastResult = getResults[getResults.length - 1];
+        const arg1 = parseFloat(firstArgument.textContent);
+        if (firstArgument.textContent == '') {
+            console.log("firstArgument is empty...");
+            if(lastResult){ 
+                arg2 = parseFloat(lastResult);
+                updateOperator(btnClass); } 
+        } else {
+            if (operator == ''){
+                arg2 = parseFloat(result);
+                printNextArg.textContent = parseFloat(firstArgument.textContent);
+                printNextArg.classList.add('result', 'top');
+                screenParent.insertBefore(printNextArg, screenSpacer);
+                updateOperator(btnClass);
+            } else {
+                const findArg2 = getResults[(getResults.length) - 1].textContent;
+                if (getResults.length == 1) {
+                    arg2 = parseFloat(findArg2);
+                    console.log("SKIP SLICE (first argument received)");
+                    doMath(arg2, arg1, operator);
+                } else {
+                    arg2 = parseFloat(result);
+                    doMath(arg2, arg1, operator);
+                }
+                printNextArg.textContent = symbol + " " + arg1;
+                printNextArg.classList.add("result");
+                screenParent.insertBefore(printNextArg, screenSpacer);
+
+                const printResult = document.createElement('p');
+                printResult.classList.add("result");
+                printResult.textContent = result;
+                screenParent.insertBefore(printResult, screenSpacer);
+
+                updateOperator(btnClass);
+            }
+        }
+    }
+} 
+
+function updateOperator(btnClass){ 
+    // console.log(`UPDATING OPERATOR ... received ${btnClass}`);
+    if(btnClass == ""){
+    } else {
+        if (btnClass == "divide") {
+            symbol = "÷";
+            operator = "divide"
+        } else if (btnClass == "multiply") {
+            symbol = "✕";
+            operator = "multiply"
+        } else if (btnClass == "subtract") {
+            symbol = "–";
+            operator = "subtract"
+        } else if (btnClass == "add") {
+            symbol = "+";
+            operator = "add"
+        } else if (btnClass == "equals"){
+            symbol = "";
+            operator = "";
+        }
+        const entry = document.getElementById('newEntry');
+        const nextArg = entry.textContent.slice(4);
+        newOp.classList.add('newOp');
+        newOp.textContent = symbol;
+        const newEntry = document.querySelector('#newEntry');
+        newEntry.prepend(newOp);
+        newEntry.append("   ");
+        firstArgument.textContent = "";
+        firstArgument.classList.add('newArg');
+        btnDecimalActive = true;
+    }
+}
+
+function fullReset(){
+    const fullResults = document.querySelectorAll('.result');
+    fullResults.forEach( (e) => { 
+        e.parentNode.removeChild(e); 
+    });
+    firstArgument.textContent = "";
+    insertOp.textContent = "";
+    btnDecimalActive = true;
+    operator = "";
+    symbol = "";
+    haveArg = false;
+    console.log("############ ALL CLEAR #############");
+}
+
+function doMath(arg2, arg1, op){
+    if (op == "divide") {
+        division(arg1, arg2);
+        console.log(`result = ${result}`);
+    } else if (op == "multiply") {
+        multiplication(arg1, arg2);
+        console.log(`result = ${result}`);
+    } else if (op == "subtract") {
+        subtraction(arg1, arg2);
+        console.log(`result = ${result}`);
+    } else if (op == "add") {
+        addition(arg1, arg2);
+        console.log(`result = ${result}`);
+    } else {
+        console.warn("NOT A VALID OPERATION");
+    }
+}
+
+// MATH FUNCTIONS
 function addition(arg1, arg2){
-    result = arg1 + arg2;
+    result = arg2 + arg1;
     return result;
 }
 
 function subtraction(arg1, arg2){
-    result = arg1 - arg2;
+    result = arg2 - arg1;
     return result;
 }
 
 function multiplication(arg1, arg2){
-    result = arg1 * arg2;
+    result = arg2 * arg1;
     return result;
 }
 
 function division(arg1, arg2){
-    result = arg1 / arg2;
+    result = arg2 / arg1;
     return result;
-}
-
-const btns = document.querySelector('#buttons');
-const nums = document.getElementById('#numbers');
-const screen = document.getElementById('screenDisplay');
-const screenSpacer = document.getElementById('screenSpacer');
-const btnRegister = document.createElement('span');
-
-const btnPress = btns.addEventListener('click', (key) => {
-    const checkFloat = parseFloat(key.target.textContent);
-    const btnClass = key.target.className;
-    console.log(checkFloat);
-    console.log(btnClass);
-    validateKeypress(checkFloat, btnClass);
-});
-    
-function validateKeypress(checkFloat, btnClass){
-    if(!btnClass){
-        btnRegister.textContent += checkFloat;
-        screen.appendChild(btnRegister);
-    } else if(btnClass == "decimal"){
-        if(btnDecimalActive == true){
-            if(btnRegister.textContent == ""){
-                btnRegister.textContent += "0.";
-                btnDecimalActive = false;
-                screen.appendChild(btnRegister);
-            } else {
-                btnRegister.textContent += ".";
-                screen.appendChild(btnRegister);
-                btnDecimalActive = false;
-            }
-        }
-    } else if(btnClass == "clear"){
-        while (screenSpacer.hasChildNodes()){
-            screenSpacer.removeChild(screenSpacer.lastChild);
-            console.log(screenSpacer.length);
-        }
-        btnRegister.textContent = "";
-        btnDecimalActive = true;
-    } else if(btnClass == "backspace"){
-        let backUp = btnRegister.textContent;
-        backUp = backUp.substring((backUp.length - 1), backUp.length);
-        if(backUp === "."){
-            btnDecimalActive = true;
-        }
-        btnRegister.textContent = btnRegister.textContent.slice(0, -1);
-        screen.appendChild(btnRegister);
-    } else if(btnClass == "equals"){
-    } else {
-        result = btnRegister.textContent;
-        updateScreen(btnClass);
-    }
-}
-
-function updateScreenArg(symbol){
-    if(haveArg == true){
-        console.log(result);
-        console.log(parseFloat(result));
-    }
-    const screenArg = document.createElement('span');
-    screenArg.textContent = parseFloat(result);
-    screenArg.textContent += `  ${symbol} `;
-    screenArg.classList.add(`${result}`);
-    screenSpacer.appendChild(screenArg);
-
-    // reset tracking variables
-    btnRegister.textContent = "";
-    haveArg = true;
-    btnDecimalActive = true;
-}
-
-function updateScreen(operator){
-    const updateScreenOp = document.createElement('span');
-    let symbol = "";
-    if(operator == "divide"){
-        symbol = "÷";
-    } else if(operator == "multiply"){
-        symbol = "✕";
-    } else if(operator == "subtract"){
-        symbol = "–";
-    } else if(operator == "add"){
-        symbol = "+";
-    }
-    updateScreenArg(symbol);
-}
-function updateScreenResult(arg){}
-
-function initNewLine(){
-    const newLine = document.createElement('span');
-    screen.appendChild(newLine);
 }
